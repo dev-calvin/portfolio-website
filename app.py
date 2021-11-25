2e009ed611260c703fab1365ccfbd36dd971fca4
Calvin McClure <calvin.mcclure0917@gmail.com>
2021-11-25 01:19:40 -0800

    added git ignore, favicon, and sample html file

--- app.py
+++ app.py
@@ -1,13 +1,24 @@
 import os
-
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+Bootstrap(app)
+
 
 @app.route('/')
+def index():
+    data = {
+        'title' : 'Calvin McClure'
+    }
+    return render_template('index.html', content=data)
+
+
+@app.route('/hello')
 def hello_world():
     target = os.environ.get('TARGET', 'World')
     return 'Hello {}!\n'.format(target)
+
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
